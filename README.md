ba340a32f8ae12fb3140e9d02b004e61afedf437
Podexus23 <santon23@gmail.com>
2026-03-16 12:56:20 +0300

    feat: add simple app logic

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # weather-api
+
+add weather endpoint
+get data
+resend data trough our api
+put town as a var
+chose current, daily, weekly
+sort/filter showed data
+
+cache requests to redis (daily)
+add rate limiter
+add axios
+add rate-limiter
+add pug to show results
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('hello');
+import { createServer } from 'node:http';
+import 'dotenv/config';
+
+const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?unitGroup=metric&key=${process.env.WEATHER_API_KEY}&contentType=json`;
+
+async function getWeather() {
+  const res = await fetch(URL);
+  if (res.ok) {
+    const json = await res.json();
+    return json;
+  }
+  return 'empty';
+}
+
+const server = createServer(async (req, res) => {
+  console.log(req.url);
+
+  if (req.url === '/api/weather' && process.env.WEATHER_API_KEY) {
+    const data = await getWeather();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data,
+      })
+    );
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data: 'Page not found',
+      })
+    );
+  }
+});
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`server listens on ${PORT} port`);
+});
